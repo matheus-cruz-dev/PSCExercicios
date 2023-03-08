f9bc3cadf72ef4ce1b2ec1aa3701888c4d6958ef
Matheus Cruz <matheus.cruz.2005@hotmail.com>
2023-03-07 21:05:06 -0300

    initial commit

---
+++ src/S001.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class S001 {
+
+    // S001 Criar um algoritmo que leia o valor de um produto e imprima na console este valor com 30% de acréscimo.
+
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+
+        
+    }
+}
